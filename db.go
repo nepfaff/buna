1e8f700d99cfd24328d81238ef6ec32924be8904
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-08-15 12:23:28 +0100

    Add TransactContext and start migrate functionality

--- db.go
+++ db.go
@@ -1,5 +1,11 @@
 package buna
 
+import (
+	"context"
+	"database/sql"
+)
+
 type DB interface {
+	TransactContext(ctx context.Context, f func(ctx context.Context, tx *sql.Tx) error) error
 	Close() error
 }
--- sqlite_db.go
+++ sqlite_db.go
@@ -25,10 +25,62 @@ func OpenSQLiteDB(ctx context.Context, logger *zap.Logger, dsn string) (*SQLiteD
 		return nil, fmt.Errorf("buna: sqlite_db: sqlite db down: %w", err)
 	}
 
-	return &SQLiteDB{
+	s := &SQLiteDB{
 		db:     db,
 		logger: logger,
-	}, nil
+	}
+
+	if err := s.migrate(ctx); err != nil {
+		s.Close()
+		return nil, fmt.Errorf("buna: sqlite_db: failed to migrate SQLite database: %w", err)
+	}
+
+	return s, nil
+}
+
+func (s *SQLiteDB) migrate(ctx context.Context) error {
+	if err := s.TransactContext(ctx, func(ctx context.Context, tx *sql.Tx) error {
+		if _, err := tx.ExecContext(ctx, `
+			CREATE TABLE IF NOT EXISTS coffees (
+				id INTEGER NOT NULL PRIMARY KEY,
+				name TEXT NOT NULL,
+				roaster TEXT NULL,
+				region TEXT NULL,
+				variety TEXT NULL,
+				method TEXT NULL,
+				decaf BOOLEAN NULL CHECK (decaf IN (0,1)),
+				UNIQUE(name, roaster)
+			)
+		`); err != nil {
+			return fmt.Errorf("buna: sqlite_db: failed to create coffees table: %w", err)
+		}
+
+		return nil
+	}); err != nil {
+		return fmt.Errorf("buna: sqlite_db: transaction failed: %w", err)
+	}
+
+	return nil
+}
+
+func (s *SQLiteDB) TransactContext(ctx context.Context, f func(ctx context.Context, tx *sql.Tx) error) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("buna: sqlite_db: failed to begin a transaction: %w", err)
+	}
+
+	defer func() {
+		if err != nil {
+			if err := tx.Rollback(); err != nil {
+				s.logger.Error("buna: sqlite_db: transaction rollback failed")
+			}
+			return
+		}
+
+		err = tx.Commit()
+	}()
+
+	return f(ctx, tx)
 }
 
 func (s *SQLiteDB) Close() error {
